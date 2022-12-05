2f655eb87c8a6246fe13a43a5bd39e8bae111c30
server <wll.rgr@gmail.com>
2022-12-05 14:00:36 -0300

    Text Blocks

---
+++ src/org/arca/textblocks/TestTextBlocks.java
@@ -0,0 +1,13 @@
+package org.arca.textblocks;
+
+public class TestTextBlocks {
+
+	public static void main(String[] args) {
+		String t = """
+				Ola \s \s Java \
+				Estamos Aqui Para Avançar
+				""";
+		
+		System.out.println(t);
+	}
+}
